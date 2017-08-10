6f64f9018ede5c9f74448c7e501e9411e8bdf2cf
Andrea0229 <cpy123go@sina.cn>
2017-08-10 14:25:42 +0800

    task0810_choose Only One number

---
+++ 0810/chooseOnlyOne.js
@@ -0,0 +1,16 @@
+
+var list = [1,1,2,2,3,4,4,7,8,9,9,8,7,10,10,90,89,78,67,78,89,90,67];
+console.log("数组"+list+"中仅出现一次的数字是："+getOnlyOne(list));
+
+function getOnlyOne(list){
+    let [single,single_sum, double_sum] = [[], 0, 0];
+    // let sum = 0
+    list.forEach(item =>{
+        if(single.indexOf(item) < 0){
+            single.push(item);
+            single_sum += item;
+        }
+        double_sum += item;
+    })
+     return single_sum*2 - double_sum;
+}
