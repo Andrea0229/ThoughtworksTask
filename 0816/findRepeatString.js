899628778dc7199177f513060398eae4f4cc5adb
Andrea0229 <cpy123go@sina.cn>
2017-08-16 17:04:12 +0800

    class exercise

--- 0816/findRepeatString.js
+++ 0816/findRepeatString.js
@@ -2,19 +2,20 @@
  题目：找出数组 arr 中重复出现过的元素
  例如：arr = [‘a’, ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
  **/
-
 let arr = ['a', 'ab', 'ed', 'da', 'a', 'ed', 'b'];
-// console.log(arr.sort())
-console.log(arr + "重复的元素有：");
-console.log(findRepeatString(arr).join(","));
+console.log(arr + "重复的元素有：" + findRepeatString(arr).join(","));
 
-
+/**
+ * 找出数组中重复的元素
+ * @param arr
+ * @returns {Array}
+ */
 function findRepeatString(arr){
     let result = [];
 
-    arr = arr.sort();                           //数组排序
+    arr = arr.sort();
     for(let i=1; i<arr.length-1; i++){
-        if(arr[i-1] === arr[i]  ||  arr[i+1] === arr[i]){
+        if((arr[i-1] === arr[i]  ||  arr[i+1] === arr[i]) && result.indexOf(arr[i]) < 0){
             result.push(arr[i])
         }
     }
---
+++ JustTesting/ClassTest/People.js
@@ -0,0 +1,18 @@
+
+/**
+ * 定义一个people类
+ */
+class People{
+    /** 构造函数 **/
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+    printMsg(){
+        console.log(this.id + ":" + this.name);
+    }
+}
+
+let p1 = new People("01", "bendan");
+console.log(typeof (p1));
+p1.printMsg();
